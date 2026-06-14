ca884ab6c6e2feb0da0cc5d05c67c3c1f6a25fda
ripe_onions <mahir.muzaddid2025@gmail.com>
2026-06-14 12:19:32 -0700

    Commit

---
+++ src/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Raylib_cs;
+using static Global;
+
+class Global // define global variables here
+{
+    // WINDOW VARIABLES
+    public const int WINW = 1024;
+    public const int WINH = 512;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        // Initialization
+        Raylib.InitWindow(WINW, WINH, "Basic Window");
+        Raylib.SetTargetFPS(60);
+        float dt;
+
+        while (!Raylib.WindowShouldClose())
+        {
+            dt = Raylib.GetFrameTime(); // delta time
+
+            // drawing
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+            Raylib.EndDrawing();
+        }
+
+        // closing and unloading assets
+        Raylib.CloseWindow();
+    }
+}
